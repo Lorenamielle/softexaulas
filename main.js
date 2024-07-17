ff8c19a463a0badbf3b1e88aef318c3c3974be9b
Lorena Mielle <miellelorena75@gmail.com>
2024-07-16 21:44:22 -0300

    atividade 1

---
+++ main.js
@@ -0,0 +1,66 @@
+// //----------------1 questao--------------------//
+// let valorCotacao = Number(prompt('Informe o valor da cotação do dolar:'));
+// let valorReal = Number(prompt('Informe seu valor em real'));
+// let valoremdolar = valorReal/valorCotacao;
+
+// alert (valoremdolar.toFixed(2));
+// console.log(valoremdolar.toFixed(2));
+// //document
+
+// //---------------2 questao-----------------//
+// let numero = Number(prompt('Informe o seu numero:'));
+
+// if (numero % 2 == 0) {
+//     alert('e par')
+// } else {
+//     alert('e impar')
+//}
+
+//-------------------3 questao--------------------//
+// let n1 = Number(prompt('Informe seu numero'));
+// let n2 = Number(prompt('Informe seu numero'));
+//  if (n1>n2){
+//     alert(`O numero ${n1} e maior que ${n2}` )
+//  } else if (n1==n2) {
+//     alert(`seus numeros sao iguais`)
+//  }else{
+//     alert(`ò numero ${n1} e menor que ${n2}`)
+//}
+
+//-------------------4 questao---------------------//
+// let n1 = Number(prompt(`Informe sua primeira nota:`));
+// let n2 = Number(prompt(`Informe sua segunda nota:`));
+// let media = (n1 + n2)/2 
+
+// if (n1>=6) {
+//     alert(`Voce foi aprovado com a media ${media}`)
+// } else {
+//     alert(`Voce foi reprovado com a media ${media}`)
+// };
+
+//---------------------5 questao-------------------------//
+// let opcao = Number(prompt(`\n1-> Ingresso VIP sem pipoca: 80 reais
+// \n2-> Ingresso VIP com pipoca: 110 reais
+// \n3-> Ingresso standard sem pipoca: 40 reais
+// \n4-> Ingresso standard com pipoca: 70 reais
+// \n Informe o seu ingresso`));
+
+// switch (opcao) {
+//     case 1:
+//         alert(`Voce escolheu o ingresso VIP com uma poltrona e sem pipoca.\nO valor foi de R$80 Reais`);
+//         break;
+//     case 2:
+//         alert(`Voce escolheu o ingresso VIP com uma poltrona e com pipoca.\nO valor foi de R$110 Reais`);
+//         break;
+//      case 3:
+//          alert(`Voce escolheu o ingresso standard com cadeira comum e sem pipoca.\nO valor foi de R$40 Reais`);
+//          break;
+//     case 4:
+//         alert(`Voce escolheu o ingresso standard cadeira comum e com pipoca.\nO valor foi de R$70 Reais`);
+//         break;
+//         default:
+//         alert(`Essa opção não existe`);
+//         break;
+// }
+
+//-----------------------6 questao---------------------//
